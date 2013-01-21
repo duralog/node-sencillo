8dc61b166ce802075a7d26179c08e95328a582eb
Xavier Mendez <jmendeth@gmail.com>
2013-01-21 18:50:29 +0100

    API: enums and flags are now wrapped in JS hashes

--- src/repository.cc
+++ src/repository.cc
@@ -30,6 +30,7 @@ using v8u::Int;
 using v8u::Symbol;
 using v8u::Bool;
 using v8u::Func;
+using v8::Local;
 
 namespace gitteh {
 
@@ -43,36 +44,44 @@ V8_ESCTOR(Repository) { V8_CTOR_NO_JS }
 NODE_ETYPE(Repository, "Repository") {
   //TODO
   
-  v8::Local<v8::Function> func = templ->GetFunction();
+  Local<v8::Function> func = templ->GetFunction();
   
   //ENUM: repository states -- STATE
-  func->Set(Symbol("STATE_NONE"), Int(GIT_REPOSITORY_STATE_NONE));
-  func->Set(Symbol("STATE_MERGE"), Int(GIT_REPOSITORY_STATE_MERGE));
-  func->Set(Symbol("STATE_REVERT"), Int(GIT_REPOSITORY_STATE_REVERT));
-  func->Set(Symbol("STATE_CHERRY_PICK"), Int(GIT_REPOSITORY_STATE_CHERRY_PICK));
-  func->Set(Symbol("STATE_BISECT"), Int(GIT_REPOSITORY_STATE_BISECT));
-  func->Set(Symbol("STATE_REBASE"), Int(GIT_REPOSITORY_STATE_REBASE));
-  func->Set(Symbol("STATE_REBASE_INTERACTIVE"), Int(GIT_REPOSITORY_STATE_REBASE_INTERACTIVE));
-  func->Set(Symbol("STATE_REBASE_MERGE"), Int(GIT_REPOSITORY_STATE_REBASE_MERGE));
-  func->Set(Symbol("STATE_APPLY_MAILBOX"), Int(GIT_REPOSITORY_STATE_APPLY_MAILBOX));
-  func->Set(Symbol("STATE_APPLY_MAILBOX_OR_REBASE"), Int(GIT_REPOSITORY_STATE_APPLY_MAILBOX_OR_REBASE));
+  Local<v8::Object> stateHash = v8u::Obj();
+  stateHash->Set(Symbol("NONE"), Int(GIT_REPOSITORY_STATE_NONE));
+  stateHash->Set(Symbol("MERGE"), Int(GIT_REPOSITORY_STATE_MERGE));
+  stateHash->Set(Symbol("REVERT"), Int(GIT_REPOSITORY_STATE_REVERT));
+  stateHash->Set(Symbol("CHERRY_PICK"), Int(GIT_REPOSITORY_STATE_CHERRY_PICK));
+  stateHash->Set(Symbol("BISECT"), Int(GIT_REPOSITORY_STATE_BISECT));
+  stateHash->Set(Symbol("REBASE"), Int(GIT_REPOSITORY_STATE_REBASE));
+  stateHash->Set(Symbol("REBASE_INTERACTIVE"), Int(GIT_REPOSITORY_STATE_REBASE_INTERACTIVE));
+  stateHash->Set(Symbol("REBASE_MERGE"), Int(GIT_REPOSITORY_STATE_REBASE_MERGE));
+  stateHash->Set(Symbol("APPLY_MAILBOX"), Int(GIT_REPOSITORY_STATE_APPLY_MAILBOX));
+  stateHash->Set(Symbol("APPLY_MAILBOX_OR_REBASE"), Int(GIT_REPOSITORY_STATE_APPLY_MAILBOX_OR_REBASE));
+  func->Set(Symbol("State"), stateHash);
 
   //FLAG: init() options -- INIT
-  func->Set(Symbol("INIT_BARE"), Int(GIT_REPOSITORY_INIT_BARE));
-  func->Set(Symbol("INIT_NO_REINIT"), Int(GIT_REPOSITORY_INIT_NO_REINIT));
-  func->Set(Symbol("INIT_NO_DOTGIT_DIR"), Int(GIT_REPOSITORY_INIT_NO_DOTGIT_DIR));
-  func->Set(Symbol("INIT_MKDIR"), Int(GIT_REPOSITORY_INIT_MKDIR));
-  func->Set(Symbol("INIT_MKPATH"), Int(GIT_REPOSITORY_INIT_MKPATH));
-  func->Set(Symbol("INIT_EXTERNAL_TEMPLATE"), Int(GIT_REPOSITORY_INIT_EXTERNAL_TEMPLATE));
+  Local<v8::Object> initFlagHash = v8u::Obj();
+  initFlagHash->Set(Symbol("BARE"), Int(GIT_REPOSITORY_INIT_BARE));
+  initFlagHash->Set(Symbol("NO_REINIT"), Int(GIT_REPOSITORY_INIT_NO_REINIT));
+  initFlagHash->Set(Symbol("NO_DOTGIT_DIR"), Int(GIT_REPOSITORY_INIT_NO_DOTGIT_DIR));
+  initFlagHash->Set(Symbol("MKDIR"), Int(GIT_REPOSITORY_INIT_MKDIR));
+  initFlagHash->Set(Symbol("MKPATH"), Int(GIT_REPOSITORY_INIT_MKPATH));
+  initFlagHash->Set(Symbol("EXTERNAL_TEMPLATE"), Int(GIT_REPOSITORY_INIT_EXTERNAL_TEMPLATE));
+  func->Set(Symbol("InitFlag"), initFlagHash);
 
   //FLAG: init() mode options -- INIT_SHARED
-  func->Set(Symbol("INIT_SHARED_UMASK"), Int(GIT_REPOSITORY_INIT_SHARED_UMASK));
-  func->Set(Symbol("INIT_SHARED_GROUP"), Int(GIT_REPOSITORY_INIT_SHARED_GROUP));
-  func->Set(Symbol("INIT_SHARED_ALL"), Int(GIT_REPOSITORY_INIT_SHARED_ALL));
+  Local<v8::Object> initModeHash = v8u::Obj();
+  initModeHash->Set(Symbol("SHARED_UMASK"), Int(GIT_REPOSITORY_INIT_SHARED_UMASK));
+  initModeHash->Set(Symbol("SHARED_GROUP"), Int(GIT_REPOSITORY_INIT_SHARED_GROUP));
+  initModeHash->Set(Symbol("SHARED_ALL"), Int(GIT_REPOSITORY_INIT_SHARED_ALL));
+  func->Set(Symbol("InitMode"), initModeHash);
 
   //FLAG: open() options -- OPEN
-  func->Set(Symbol("OPEN_NO_SEARCH"), Int(GIT_REPOSITORY_OPEN_NO_SEARCH));
-  func->Set(Symbol("OPEN_CROSS_FS"), Int(GIT_REPOSITORY_OPEN_CROSS_FS));
+  Local<v8::Object> openFlagHash = v8u::Obj();
+  openFlagHash->Set(Symbol("NO_SEARCH"), Int(GIT_REPOSITORY_OPEN_NO_SEARCH));
+  openFlagHash->Set(Symbol("CROSS_FS"), Int(GIT_REPOSITORY_OPEN_CROSS_FS));
+  func->Set(Symbol("OpenFlag"), openFlagHash);
 
 } NODE_TYPE_END()
 V8_POST_TYPE(Repository)
