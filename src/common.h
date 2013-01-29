c44a546d08af705b5ea41e8f60232288df86d85f
Xavier Mendez <jmendeth@gmail.com>
2013-01-29 21:28:07 +0100

    Remodelate async macros; fix some leaks
    Asynchronous wrapping can now be considered stable.

--- src/common.h
+++ src/common.h
@@ -48,12 +48,29 @@ V8_SCB(_isAbstract);
 // --based on node-lame's code
 
 //Unwrapping and starting work statement-macros
+#define GITTEH_ASYNC_CSTR(OBJ, VAR)                                            \
+  int len = OBJ->length();                                                     \
+  char* VAR = new char[len+1];                                                 \
+  memcpy(VAR, **OBJ, len);                                                     \
+  VAR[len] = 0
+#define GITTEH_SYNC_CSTR(OBJ, VAR)                                             \
+  int len = OBJ.length();                                                      \
+  char* VAR = new char[len+1];                                                 \
+  memcpy(VAR, *OBJ, len);                                                      \
+  VAR[len] = 0
+
 #define GITTEH_WORK_UNWRAP(IDENTIFIER)                                         \
   IDENTIFIER##_req* r = (IDENTIFIER##_req*)req->data
 #define GITTEH_WORK_QUEUE(IDENTIFIER)                                          \
   r->req.data = r;                                                             \
   return v8::Integer::New(uv_queue_work(uv_default_loop(), &r->req,            \
                                         IDENTIFIER##_work, IDENTIFIER##_after))
+#define GITTEH_WORK_CALL(ARGC)                                                 \
+  v8::TryCatch try_catch;                                                      \
+  r->cb->Call(v8::Context::GetCurrent()->Global(), ARGC, argv);                \
+  r->cb.Dispose();                                                             \
+  delete r;                                                                    \
+  if (try_catch.HasCaught()) node::FatalException(try_catch)
 
 
 //Work callbacks block-macros
@@ -71,13 +88,7 @@ V8_SCB(_isAbstract);
     v8::HandleScope scope;                                                     \
     GITTEH_WORK_UNWRAP(IDENTIFIER);
 
-#define GITTEH_WORK_END(ARGC)                                                  \
-  v8::TryCatch try_catch;                                                      \
-  r->cb->Call(v8::Context::GetCurrent()->Global(), ARGC, argv);                \
-  r->cb.Dispose();                                                             \
-  delete r;                                                                    \
-  if (try_catch.HasCaught()) node::FatalException(try_catch);                  \
-}
+#define GITTEH_END }
 
 #define GITTEH_CHECK_CB_ARGS(MIN)                                              \
   if (len < 1) V8_STHROW(v8u::RangeErr("Not enough arguments!"));
