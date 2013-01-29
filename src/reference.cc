825152bf7c7466480e3ffa96a20eed45e40ff9e7
Xavier Mendez <jmendeth@gmail.com>
2013-01-29 11:59:53 +0100

    reference:: Add files for it.

---
+++ src/reference.cc
@@ -0,0 +1,45 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2010 Sam Day
+ * Copyright (c) 2012 Xavier Mendez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "reference.h"
+
+#include "common.h"
+
+
+namespace gitteh {
+
+// Deny instances
+V8_ESCTOR(Reference) { V8_CTOR_NO_ALL }
+
+NODE_ETYPE(Reference, "Reference") {
+  V8_DEF_CB("peel", _isAbstract);
+  V8_DEF_CB("peelSync", _isAbstract);
+  //Damn it, if there was a way to abstract "properties"...
+} NODE_TYPE_END()
+
+V8_POST_TYPE(Reference)
+
+};
+
