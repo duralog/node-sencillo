813a3d3839c2c3970cf5293eda9760d2fa022dbc
Xavier Mendez <jmendeth@gmail.com>
2013-01-31 16:34:18 +0100

    Reference:: add resolve(...)

--- src/reference.cc
+++ src/reference.cc
@@ -28,6 +28,7 @@
 #include "repository.h"
 #include "common.h"
 #include "error.h"
+#include "oid.h"
 
 
 using v8u::Int;
@@ -97,6 +98,50 @@ V8_SCB(Reference::Lookup) {
 } GITTEH_END
 
 
+//// Reference.resolve(...)
+
+GITTEH_WORK_PRE(ref_sresolve) {
+  v8::String::Utf8Value* name;
+  git_repository* repo;
+  git_oid out; bool ok;
+  error_info err;
+
+  Persistent<Function> cb;
+  uv_work_t req;
+};
+
+V8_SCB(Reference::StaticResolve) {
+  v8::Local<v8::Object> repo_obj;
+  if (!(args[0]->IsObject() && Repository::HasInstance(repo_obj = v8u::Obj(args[0]))))
+    V8_STHROW(v8u::TypeErr("Repository needed as first argument."));
+  if (!args[2]->IsFunction()) V8_STHROW(v8u::TypeErr("A Function is needed as callback!"));
+
+  ref_sresolve_req* r = new ref_sresolve_req;
+  r->repo = node::ObjectWrap::Unwrap<Repository>(repo_obj)->repo;
+  r->name = new v8::String::Utf8Value(args[1]);
+
+  r->cb = v8u::Persist<Function>(v8u::Cast<Function>(args[2]));
+  GITTEH_WORK_QUEUE(ref_sresolve);
+} GITTEH_WORK(ref_sresolve) {
+  GITTEH_ASYNC_CSTR(r->name, cname);
+
+  int status = git_reference_name_to_id(&r->out, r->repo, cname);
+  delete [] cname;
+  if ((r->ok= status == GIT_OK)) return;
+  collectErr(status, r->err);
+} GITTEH_WORK_AFTER(ref_sresolve) {
+  v8::Handle<v8::Value> argv [2];
+  if (r->ok) {
+    argv[0] = v8::Null();
+    argv[1] = (new Oid(r->out))->Wrapped();
+  } else {
+    argv[0] = composeErr(r->err);
+    argv[1] = v8::Null();
+  }
+  GITTEH_WORK_CALL(2);
+} GITTEH_END
+
+
 
 NODE_ETYPE(Reference, "Reference") {
   //TODO
@@ -106,6 +151,8 @@ NODE_ETYPE(Reference, "Reference") {
   func->Set(Symbol("lookup"), Func(Lookup)->GetFunction());
 //  func->Set(Symbol("lookupSync"), Func(LookupSync)->GetFunction());
   
+  func->Set(Symbol("resolve"), Func(StaticResolve)->GetFunction());
+//  func->Set(Symbol("resolveSync"), Func(StaticResolveSync)->GetFunction());
 } NODE_TYPE_END()
 
 V8_POST_TYPE(Reference)
